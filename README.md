c2083f3339f4c6405a001ac29f49d24e095ac4d6
Dillon_Pullano <132423399+dgpullano@users.noreply.github.com>
2023-06-28 16:13:10 -0600

    Added screenshot, MD file, and finalized code for submission

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Lab2_20230628
  Submission files for ENSF 694 - Lab # 2
+ 
+ Please download the Github repository submitted through D2L to get all submitted files.
+ 
+# Author:
+DILLON PULLANO
+
+# Question 1 (Sample Output):
+![Sample Output from Program](Sample_Output_Screenshot.jpg)
+
+# Question 2:
+For the example shown in the added screenshot:
+   - Linear search took 2900 nanoseconds
+   - Interpolation search took 2700 nanoseconds
+   
+Interpolation was a little faster than linear. However, this difference increases when a non-sorted array is used with the linear method.
+
+It makes sense that interpolated should be faster since it takes a weighted approximated guess to reach a result instead of just linearly progressing through the data.
+
+
+# Question 3:
+My improved version of the linear search algorithm makes the assumption that the array is first sorted. 
+   - Firstly, the algorithm checks the search key against the middle index value, similar to binary search. 
+   - If the search key is lower than the middle index, a linear search is done in the negative direction from the middle index to the start of the array.
+   - If the search key is higher than the middle index, a linear search is done in the positive direction from the middle index to the end of the array.
+   
+Using the sample output shown in the added screenshot, this change brought the runtime down from 2900 nanoseconds to 1800 nanoseconds. 
+   - Runtime cut down to approximately 62% of original by making this improvement
+   - This is an improvement of approximately 38%
--- src/Lab2_Solution.java
+++ src/Lab2_Solution.java
@@ -1,21 +1,46 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-
-
-
+/**
+ * This class implements the necessary functions to complete 
+ * Q1, Q2, and Q3 for the Lab 2 assignment.
+ * 
+ * @author Dillon Pullano
+ *
+ */
 public class Lab2_Solution {
 	
+	/**
+	 * Implements original linear search algorithm.
+	 * @param array
+	 * @param key
+	 * @return array index of first found key match
+	 */
 	public static int linearSearch(int[] array, int key) {
+		
+		// Pass through array linearly until key is matched or end of array is reached:
 		for(int i = 0; i < array.length; i++) {
 			if(array[i] == key) {
+				
+				// Matching index:
 				return i;
 			}
 		}
+		
+		// No matching index exists:
 		return -1;
 	}
 	
+	
+	/**
+	 * Implements interpolation search algorithm.
+	 * @param array
+	 * @param key
+	 * @return array index of first found key match
+	 */
 	public static int interpolationSearch(int[] array, int key) {
+		
+		//Initialize intermediate parameters:
 		int low = 0;
 		int high = array.length - 1;
 		int mid;
@@ -23,15 +48,21 @@ public class Lab2_Solution {
 		
 		while(low <= high) {
 
+			// Get new interpolated index for expected location of match:
 			pos = (key - array[low]) / (array[high] - array[low]);
 			mid = low + (((high - low) * pos));
 			
+			// When key is likely to be lower than mid:
 			if(key < array[mid]) {
 				high = mid - 1;
 			}
+			
+			// When key is likely to be higher than mid:
 			else if(array[mid] < key) {
 				low = mid + 1;
 			}
+			
+			// When there is match on first try:
 			else {
 				return mid;
 			}
@@ -39,13 +70,65 @@ public class Lab2_Solution {
 		return -1;
 	}
 	
+	
+	/**
+	 * Implements optimized linear search algorithm. 
+	 * @param array
+	 * @param key
+	 * @return array index of first found key match
+	 */
+	public static int linearSearchOptimized(int[] array, int key) {
+		
+		// Calculate middle index:
+		int mid = (array.length - 1) / 2;
+		
+		// When there is match on first try:
+		if(key == array[mid]) {
+			return mid;
+		}
+		
+		// Progress linearly from middle index in the negative direction to the start of array:
+		else if (key < array[mid]) {
+			for(int i = mid; i >= 0; i--) {
+				if(array[i] == key) {
+					return i;
+				}
+			}
+			return -1;
+		}
+		
+		// Progress linearly from middle index in the positive direction to the end of array:
+		else if(key >= array[mid]) {
+			for(int j = mid; j <= array.length -1; j++) {
+				if(array[j] == key) {
+					return j;
+				}
+			}
+			return -1;
+		}
+		
+		// No match case:
+		else {
+			return -1;
+		}
+	}	
+	
+	
+	/**
+	 * Gathers user input, implements algorithms, and compares run times
+	 * @param args
+	 */
 	public static void main(String[] args) {
 
+		//-----------------------------------------------------------------------------------
+		// Part 1 - Get user inputs and store values for array and key:
+		//-----------------------------------------------------------------------------------
+		
 		// Create scanner for reading in user input:
 		Scanner input = new Scanner(System.in);
 		
 		// Ask user for length of array and store value:
-		System.out.println("Enter the number of elements in the array: ");
+		System.out.print("Enter the number of elements in the array: ");
 		int arrayLength = input.nextInt();
 		
 		// Initialize empty array:
@@ -59,36 +142,22 @@ public class Lab2_Solution {
 		}
 		
 		// Ask user for the search key and store:
-		System.out.println("Enter the search key: ");
+		System.out.print("Enter the search key: ");
 		int key = input.nextInt();
 		
-		
-		// Run the data through linear search (NON SORTED ARRAY):
-		long linStartTimeUnsorted = System.nanoTime();
-		int linSearchUnsorted = linearSearch(array, key);
-		long linEndTimeUnsorted = System.nanoTime();
-		
-		if(linSearchUnsorted == -1) {
-			System.out.println("Search key NOT FOUND");
-		}
-		else {
-			System.out.println("Search key FOUND at index " + linSearchUnsorted);
-		}
-		long linRuntimeUnsorted = linEndTimeUnsorted - linStartTimeUnsorted;
-		System.out.println("Linear search runtime (BEFORE SORTING): " + linRuntimeUnsorted);		
-		
-		
-		
-		
-		
+		// Sort array before passing through all algorithms:
 		Arrays.sort(array);
 		
 		
-		
-		// Run the data through linear search (SORTED ARRAY):
+		//-----------------------------------------------------------------------------------
+		// Part 2 - Run the data through the original linear search algorithm:
+		//-----------------------------------------------------------------------------------
 		long linStartTime = System.nanoTime();
 		int linSearch = linearSearch(array, key);
 		long linEndTime = System.nanoTime();
+		
+		System.out.println("");
+		System.out.println("Using ORIGINAL Linear Search: ");
 		
 		if(linSearch == -1) {
 			System.out.println("Search key NOT FOUND");
@@ -97,14 +166,18 @@ public class Lab2_Solution {
 			System.out.println("Search key FOUND at index " + linSearch);
 		}
 		long linRuntime = linEndTime - linStartTime;
-		System.out.println("Linear search runtime (AFTER SORTING): " + linRuntime);
+		System.out.println("ORIGINAL Linear search runtime: " + linRuntime);
 		
 		
-		
-		// Run the data through interpolation search (SORTED ARRAY):
+		//-----------------------------------------------------------------------------------
+		// Part 3 - Run the data through the interpolation search algorithm:
+		//-----------------------------------------------------------------------------------
 		long interpStartTime = System.nanoTime();
 		int interpSearch = interpolationSearch(array, key);
 		long interpEndTime = System.nanoTime();
+		
+		System.out.println("");
+		System.out.println("Using Interpolation Search: ");
 		
 		if(interpSearch == -1) {
 			System.out.println("Search key NOT FOUND");
@@ -114,6 +187,27 @@ public class Lab2_Solution {
 		}
 		long interpRuntime = interpEndTime - interpStartTime;
 		System.out.println("Interpolation search runtime: " + interpRuntime);
+		
+		
+		//-----------------------------------------------------------------------------------
+		// Part 4 - Run the data through the OPTIMIZED linear search algorithm:
+		//-----------------------------------------------------------------------------------
+		long linStartTimeOp = System.nanoTime();
+		int linSearchOp = linearSearchOptimized(array, key);
+		long linEndTimeOp = System.nanoTime();
+		
+		System.out.println("");
+		System.out.println("Using OPTIMIZED Linear Search: ");
+		
+		if(linSearchOp == -1) {
+			System.out.println("Search key NOT FOUND");
+		}
+		else {
+			System.out.println("Search key FOUND at index " + linSearchOp);
+		}
+		long linRuntimeOp = linEndTimeOp - linStartTimeOp;
+		System.out.println("OPTIMIZED Linear search runtime: " + linRuntimeOp);		
+		
 		
 		// Close scanner:
 		input.close();
